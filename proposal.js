e09b1130228f2f3c6d5f19dd7a43a7108344b2a5
aaron <aaronkirui15@gmail.com>
2026-02-08 19:49:08 +0300

    vercel new_final2.2

--- proposal.js
+++ proposal.js
@@ -1,7 +1,7 @@
 console.log('✅ proposal.js script loaded and is executing.');
 
 // ===== EDITABLE: Change the name here =====
-let personName = 'Mitchell🌹 S'; // Change this to the person's name
+let personName = 'Mitchelle🌹'; // Change this to the person's name
 // ==========================================
 let noClickCount = 0;
 
